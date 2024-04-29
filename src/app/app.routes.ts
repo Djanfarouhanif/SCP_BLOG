46fa888c5339e02c458431bfcd5ec401d22f100d
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-04-29 21:07:07 +0000

    home page code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { ArticleDetailsComponent } from './article-details/article-details.component';
-
+import { ArticleComponent } from './article/article.component'
 
 
 export const routes: Routes = [
 
     { 'path' : '', component: ArticleDetailsComponent},
+    { 'path' : 'article-detaile',  component: ArticleComponent},
    
     
 ];
+
+
--- src/app/article-details/article-details.component.ts
+++ src/app/article-details/article-details.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router'
+import { NavBarComponent } from '../nav-bar/nav-bar.component'
+import { FilterComponent } from '../filter/filter.component'
+import { FooterComponent } from '../footer/footer.component'
+
 
 @Component({
   selector: 'app-article-details',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NavBarComponent  , FilterComponent , FooterComponent],
   templateUrl: './article-details.component.html',
   styleUrl: './article-details.component.css'
 })
