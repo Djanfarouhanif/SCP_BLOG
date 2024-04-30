c04cad1b4609b6c4e3d4b8558baa2f927285059d
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-04-30 17:10:43 +0000

    add the login page

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { NavBarComponent } from '../nav-bar/nav-bar.component'
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
