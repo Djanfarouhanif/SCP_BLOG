100be655669fbaba5701b0817b24d866483cf897
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-04-30 16:06:56 +0000

    definition the route for login and signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from '@angular/router';
 import { ArticleDetailsComponent } from './article-details/article-details.component';
-import { ArticleComponent } from './article/article.component'
+import { ArticleComponent } from './article/article.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 export const routes: Routes = [
 
     { 'path' : '', component: ArticleDetailsComponent},
     { 'path' : 'article-detaile',  component: ArticleComponent},
-   
+    { 'path': 'signup', component: SignupComponent},
+    { 'path': 'login', component: SigninComponent}
     
 ];
 
