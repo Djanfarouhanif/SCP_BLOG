5b5c57c2390652a9528148620871652a7b5c5191
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-04-30 23:25:10 +0000

    navbar position after click menu

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,21 +1,30 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common'
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
     menuValue:boolean = false;
-    menu_icon:string = 'black'
+    menu_icon:string = 'menu';
+    isMenuOpen:boolean = false
 
     openMenu(){
       this.menuValue=!this.menuValue;
-      
+      this.menu_icon = this.menuValue? 'close': 'menu';
+      this.isMenuOpen = !this.isMenuOpen;
+
+
     }
-      
+    closeMenu(){
+      this.menuValue = false;
+      this.menu_icon = "menu";
+      this.isMenuOpen = false;
+  
+    }
       
 }
 
