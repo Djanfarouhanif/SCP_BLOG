2a08141665fdd01147088ac035171a4c96b095a7
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-05-02 23:32:04 +0000

    service add

--- src/app/article-details/article-details.component.ts
+++ src/app/article-details/article-details.component.ts
@@ -3,25 +3,29 @@ import { RouterLink } from '@angular/router'
 import { NavBarComponent } from '../nav-bar/nav-bar.component'
 import { FilterComponent } from '../filter/filter.component'
 import { FooterComponent } from '../footer/footer.component'
-import { ArticleService } from './article.service'
+import { ArticleService } from '../article.service'
 import { CommonModule }  from '@angular/common'
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-article-details',
   standalone: true,
-  imports: [CommonModule ,RouterLink, NavBarComponent  , FilterComponent , FooterComponent],
+  imports: [ CommonModule ,RouterLink, NavBarComponent  , FilterComponent , FooterComponent, HttpClientModule],
   templateUrl: './article-details.component.html',
-  styleUrl: './article-details.component.css'
+  styleUrl: './article-details.component.css',
+  providers: [ArticleService]
 })
 export class ArticleDetailsComponent {
-    // articles: any[] = [];
+    articles: any[] = [];
 
-    // constructor(private articleService:ArticleService){}
+    constructor(private articleService:ArticleService){}
 
-    // ngOnInit(): void {
-    //   this.articleService.getArticles().subscribe(data=>{
-    //     this.articles = data
-    //   })
-    // }
+    ngOnInit(): void {
+        this.articleService.getArticles().subscribe(data=>{
+          this.articles = data
+       })
+
+       console.log(this.articles)
+         }
 
 }
