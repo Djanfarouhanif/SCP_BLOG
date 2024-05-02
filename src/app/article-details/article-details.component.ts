999c0cec52d2499a5f2f9d9a5d22e08f8ba59c0b
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-05-02 00:07:39 +0000

    backend api rest

--- src/app/article-details/article-details.component.ts
+++ src/app/article-details/article-details.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router'
 import { NavBarComponent } from '../nav-bar/nav-bar.component'
 import { FilterComponent } from '../filter/filter.component'
 import { FooterComponent } from '../footer/footer.component'
-
+import { ArticleService } from './article.service'
+import { CommonModule }  from '@angular/common'
 
 @Component({
   selector: 'app-article-details',
   standalone: true,
-  imports: [RouterLink, NavBarComponent  , FilterComponent , FooterComponent],
+  imports: [CommonModule ,RouterLink, NavBarComponent  , FilterComponent , FooterComponent],
   templateUrl: './article-details.component.html',
   styleUrl: './article-details.component.css'
 })
 export class ArticleDetailsComponent {
+    // articles: any[] = [];
+
+    // constructor(private articleService:ArticleService){}
+
+    // ngOnInit(): void {
+    //   this.articleService.getArticles().subscribe(data=>{
+    //     this.articles = data
+    //   })
+    // }
 
 }
